26fe3d7964b3909938907fc6f2cc171568691252
babinicz <babinicz@onet.pl>
2022-03-19 13:01:24 +0100

    cookies

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Services from "./components/pages/Services";
 import Products from "./components/pages/Products";
 import SignUp from "./components/pages/SignUp";
 import ScrollToTop from "./components/ScrollToTop";
+import CookieConsent from "react-cookie-consent";
 
 function App() {
   return (
@@ -20,6 +21,33 @@ function App() {
           <Route path="/zbrodnie" exact component={SignUp} />
         </Switch>
       </Router>
+      <CookieConsent
+        debug={true}
+        location="bottom"
+        style={{
+          background: "#333",
+          textAlign: "left",
+          paddingBottom: "1rem",
+          fontSize: "16px",
+          fontFamily: "Gideon Roman",
+        }}
+        buttonStyle={{
+          color: "#333",
+          background: "#fff",
+          fontSize: "18px",
+          fontFamily: "Gideon Roman",
+          marginRight: "1rem",
+        }}
+        buttonText="OK, rozumiem"
+        expires={365}
+      >
+        "W ramach naszej witryny stosujemy pliki cookies w celu świadczenia
+        Państwu usług na najwyższym poziomie, w tym w sposób dostosowany do
+        indywidualnych potrzeb. Korzystanie z witryny bez zmiany ustawień
+        dotyczących cookies oznacza, że będą one zamieszczane w Państwa
+        urządzeniu końcowym. Możecie Państwo dokonać w każdym czasie zmiany
+        ustawień dotyczących cookies."
+      </CookieConsent>
     </>
   );
 }
