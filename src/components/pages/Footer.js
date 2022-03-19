8b057bbfaf9cf7734c324460bc99bafe1e4e1f85
babinicz <babinicz@onet.pl>
2022-03-19 15:09:29 +0100

    adresy

--- src/components/pages/Footer.js
+++ src/components/pages/Footer.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./Footer.css";
+import "../../App.css";
 import { Link } from "react-router-dom";
 import { HiAcademicCap } from "react-icons/hi";
 import { BsFacebook, BsInstagram, BsYoutube, BsTwitter } from "react-icons/bs";
+import { ImNewspaper, ImFilm } from "react-icons/im";
+import { BiLinkAlt } from "react-icons/bi";
 
 function Footer() {
   return (
@@ -14,41 +17,143 @@ function Footer() {
         <p className="footer-subscription-text">
           wojsko agresora jest odpowiedzialne za zbrodnie wojenne, a&nbsp;także
           "świadome wywołanie katastrofy humanitarnej" w&nbsp;ukraińskich
-          miastach
+          miastach!
         </p>
       </section>
       <div className="footer-links">
         <div className="footer-link-wrapper">
           <div class="footer-link-items">
             <h2>Artykuły</h2>
-            <Link to="/">x</Link>
-            <Link to="/">x</Link>
-            <Link to="/">x</Link>
-            <Link to="/">x</Link>
-            <Link to="/">x</Link>
+            <Link
+              to="//wydarzenia.interia.pl/raporty/raport-ukraina-rosja/aktualnosci/news-wojna-w-ukrainie-to-dopiero-poczatek-te-kraje-boja-sie-ze-sa,nId,5898933"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <ImNewspaper />
+            </Link>
+            <Link
+              to="//www.rp.pl/spoleczenstwo/art35889931-wojna-na-ukrainie-rosjanie-zaczynaja-miec-watpliwosci-odpowiedzi-szukaja-w-internecie"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <ImNewspaper />
+            </Link>
+            <Link
+              to="//www.polskieradio.pl/wojna-na-Ukrainie/Tag179372"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <ImNewspaper />
+            </Link>
+            <Link
+              to="//pl.wikipedia.org/wiki/Wojna_w_Donbasie"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <ImNewspaper />
+            </Link>
+            <Link
+              to="//300gospodarka.pl/news/wojna-w-ukrainie-najnowsze-informacje-na-zywo"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <ImNewspaper />
+            </Link>
           </div>
           <div class="footer-link-items">
             <h2>Linki</h2>
-            <Link to="/">x</Link>
-            <Link to="/">x</Link>
-            <Link to="/">x</Link>
-            <Link to="/">x</Link>
+            <Link
+              to="//www.tvp.info/59134070/kiedy-skonczy-sie-wojna-na-ukrainie-doradca-zelenskiego-zostalo-im-moze-14-dni-wojsko-rosyjskie-traci-potencjal-ofensywny"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <BiLinkAlt />
+            </Link>
+            <Link
+              to="//bezprawnik.pl/tag/wojna-na-ukrainie/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <BiLinkAlt />
+            </Link>
+            <Link
+              to="//forsal.pl/raporty/konflikt-rosja-ukraina"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <BiLinkAlt />
+            </Link>
+            <Link
+              to="//wiadomosci.radiozet.pl/tags/kategoria/wojna-na-ukrainie"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <BiLinkAlt />
+            </Link>
           </div>
         </div>
         <div className="footer-link-wrapper">
           <div class="footer-link-items">
             <h2>Filmy</h2>
-            <Link to="/">x</Link>
-            <Link to="/">x</Link>
-            <Link to="/">x</Link>
-            <Link to="/">x</Link>
+            <Link
+              to="//www.youtube.com/watch?v=9K7rnoU0kfQ"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <ImFilm />
+            </Link>
+            <Link
+              to="//www.youtube.com/watch?v=CUSRg7NX0w8"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <ImFilm />
+            </Link>
+            <Link
+              to="//www.youtube.com/watch?v=tq81VTcl-hs"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <ImFilm />
+            </Link>
+            <Link
+              to="//www.youtube.com/watch?v=aKMReaO-HRc"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <ImFilm />
+            </Link>
           </div>
           <div class="footer-link-items">
             <h2>Media</h2>
-            <Link to="/">Instagram</Link>
-            <Link to="/">Facebook</Link>
-            <Link to="/">Youtube</Link>
-            <Link to="/">Twitter</Link>
+            <Link
+              to="//www.instagram.com/istories.media"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Instagram
+            </Link>
+            <Link
+              to="//www.facebook.com/profile.php?id=100007502733594"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Facebook
+            </Link>
+            <Link
+              to="//www.youtube.com/watch?v=fNx8QE6jQ9M"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Youtube
+            </Link>
+            <Link
+              to="//twitter.com/istories_media"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Twitter
+            </Link>
           </div>
         </div>
       </div>
@@ -68,6 +173,7 @@ function Footer() {
               to="//www.facebook.com/profile.php?id=100007502733594"
               target="_blank"
               aria-label="Facebook"
+              rel="noopener noreferrer"
             >
               <BsFacebook />
             </Link>
@@ -76,6 +182,7 @@ function Footer() {
               to="//www.instagram.com/istories.media"
               target="_blank"
               aria-label="Instagram"
+              rel="noopener noreferrer"
             >
               <BsInstagram />
             </Link>
@@ -84,6 +191,7 @@ function Footer() {
               to="//www.youtube.com/watch?v=fNx8QE6jQ9M"
               target="_blank"
               aria-label="Youtube"
+              rel="noopener noreferrer"
             >
               <BsYoutube />
             </Link>
@@ -92,6 +200,7 @@ function Footer() {
               to="//twitter.com/istories_media"
               target="_blank"
               aria-label="Twitter"
+              rel="noopener noreferrer"
             >
               <BsTwitter />
             </Link>
